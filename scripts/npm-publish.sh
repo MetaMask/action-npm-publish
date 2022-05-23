da76fc05a1f68b0b35293e85dcf40f2770d70272
Ricky Miller <ricky.miller@gmail.com>
2022-05-23 12:43:20 -0400

    shellcheck doesn't like backticks

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,7 +5,7 @@ set -e
 set -o pipefail
 
 if [[ -z $NPM_TOKEN ]]; then
-  echo "Notice: NPM_TOKEN environment variable not set. Running `npm publish --dry-run`."
+  echo "Notice: NPM_TOKEN environment variable not set. Running 'npm publish --dry-run'."
   npm publish --dry-run
   exit 0
 fi
