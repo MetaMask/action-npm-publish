9f94769f4fb1f980f5ab0619206fb8fba53a93ab
Ricky Miller <ricky.miller@gmail.com>
2022-05-23 12:39:23 -0400

    address nits

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,7 +5,7 @@ set -e
 set -o pipefail
 
 if [[ -z $NPM_TOKEN ]]; then
-  echo "Notice: NPM_TOKEN environment variable not set. Performing --dry-run publish."
+  echo "Notice: NPM_TOKEN environment variable not set. Running `npm publish --dry-run`."
   npm publish --dry-run
   exit 0
 fi
