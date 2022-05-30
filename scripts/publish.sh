c6fdf918443968274f2b0e156f2485b3097960c7
Ricky Miller <ricky.miller@gmail.com>
2022-05-30 14:54:53 -0400

    monorepo
    This adds support for publishing monorepos. I've also standardised some things (eslint) and added some "happy path" tests to ensure everything is working as expected and we remain backwards compatible.
    
    closes: #2

---
+++ scripts/publish.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -o pipefail
+
+if [[ -z $NPM_TOKEN ]]; then
+  echo "Notice: NPM_TOKEN environment variable not set. Running 'npm publish --dry-run'."
+  npm publish --dry-run
+  exit 0
+fi
+
+# check param, if it's set (monorepo) we check if it's published before proceeding
+if [[ -n "$1" ]]; then
+  # check if module is published
+  LATEST_PACKAGE_VERSION=$(npm view . version || echo "")
+  CURRENT_PACKAGE_VERSION=$(jq --raw-output .version package.json)
+
+  if [ "$LATEST_PACKAGE_VERSION" = "$CURRENT_PACKAGE_VERSION" ]; then
+    echo "Notice: This module is already published at this version. aborting publish."
+    exit 0
+  fi
+fi
+
+npm publish
