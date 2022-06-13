a052f906e86be9d3bc29dbdee2122e70cd5ac7f1
Ricky Miller <ricky.miller@gmail.com>
2022-06-13 11:53:45 -0400

    add check for if module has been published

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,5 +10,18 @@ if [[ -z $NPM_TOKEN ]]; then
   exit 0
 fi
 
-# TODO: detect if module has been published
+# Get the published version
+# If the package is not published, set the published version to "NULL"
+NPM_VERSION=$(npm show . version || echo "NULL")
+
+# Get the local version of the package, from package.json
+# The jq "r" flag gives us the raw, unquoted output
+LOCAL_VERSION=$(jq -r .version < package.json)
+
+# Skip Publish if this version is already published
+if [[ $NPM_VERSION != "NULL" || $LOCAL_VERSION == "$NPM_VERSION" ]]; then
+  echo "Notice: This module has already been published at this verion. Aborting publish."
+  echo 0
+fi
+
 npm publish
