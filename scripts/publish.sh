d88a63c6c13f1f7f84aa18110733a562d0f401de
Ricky Miller <ricky.miller@gmail.com>
2022-07-18 19:39:15 -0400

    housekeeping

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,10 @@ set -x
 set -e
 set -o pipefail
 
+semver_to_nat () {
+  echo "${1//./}"
+}
+
 if [[ -z $NPM_TOKEN ]]; then
   echo "Notice: NPM_TOKEN environment variable not set. Running 'npm publish --dry-run'."
   npm publish --dry-run
@@ -15,9 +19,15 @@ if [[ -n "$1" ]]; then
   # check if module is published
   LATEST_PACKAGE_VERSION=$(npm view . version --workspaces=false || echo "")
   CURRENT_PACKAGE_VERSION=$(jq --raw-output .version package.json)
+  # convert to natural numbers for below comparison
+  LATEST_PACKAGE_VERSION_NAT=$(semver_to_nat "$LATEST_PACKAGE_VERSION")
+  CURRENT_PACKAGE_VERSION_NAT=$(semver_to_nat "$CURRENT_PACKAGE_VERSION")
 
-  if [ "$LATEST_PACKAGE_VERSION" = "$CURRENT_PACKAGE_VERSION" ]; then
-    echo "Notice: This module is already published at $CURRENT_PACKAGE_VERSION. aborting publish."
+  if [ "$LATEST_PACKAGE_VERSION_NAT" -ge "$CURRENT_PACKAGE_VERSION_NAT" ]; then
+    echo -e \
+      "Notice: This module cannot be published at v$CURRENT_PACKAGE_VERSION." \
+      "This module has been already published at v$LATEST_PACKAGE_VERSION." \
+      "aborting publish."
     exit 0
   fi
 fi
