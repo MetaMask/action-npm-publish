36dd576230eab5eaa4c833ca56308005d8ab981e
Ricky Miller <ricky.miller@gmail.com>
2022-12-16 00:25:49 -0500

    this needs to be -f

--- .github/workflows/scripts/update-major-version-tag.sh
+++ .github/workflows/scripts/update-major-version-tag.sh
@@ -16,14 +16,8 @@ MAJOR_VERSION_TAG="v${RELEASE_VERSION/\.*/}"
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-if git show-ref --tags "$MAJOR_VERSION_TAG" --quiet; then
-  echo "Tag ${MAJOR_VERSION_TAG} exists, attempting to replace it."
-else 
-  echo "Tag ${MAJOR_VERSION_TAG} does not exist, creating it from scratch."
-fi
-
 git tag -f "$MAJOR_VERSION_TAG" HEAD
-git push --force-with-lease origin "$MAJOR_VERSION_TAG"
+git push -f origin "$MAJOR_VERSION_TAG"
 echo "Updated shorthand major version tag."
 
 echo "MAJOR_VERSION_TAG=$MAJOR_VERSION_TAG" >> "$GITHUB_OUTPUT"
