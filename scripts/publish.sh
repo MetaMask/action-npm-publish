566cd3d45cd317100a238b37c2aa48ba66c5e33d
ricky <ricky.miller@gmail.com>
2024-03-15 11:45:38 -0400

    Disallow publishing packages with version 0.0.0 (#63)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,12 +15,18 @@ if [[ -z $PUBLISH_NPM_TAG ]]; then
   exit 1
 fi
 
+CURRENT_PACKAGE_VERSION=$(jq --raw-output .version package.json)
+
+if [[ "$CURRENT_PACKAGE_VERSION" = "0.0.0" ]]; then
+  echo "Notice: Invalid version: $CURRENT_PACKAGE_VERSION. aborting publish."
+  exit 0
+fi
+
 # check param, if it's set (monorepo) we check if it's published before proceeding
 if [[ -n "$1" ]]; then
   # check if module is published
   PACKAGE_NAME=$(jq --raw-output .name package.json)
   LATEST_PACKAGE_VERSION=$(npm view "$PACKAGE_NAME" dist-tags --workspaces false --json | jq --raw-output --arg tag "$PUBLISH_NPM_TAG" '.[$tag]' || echo "")
-  CURRENT_PACKAGE_VERSION=$(jq --raw-output .version package.json)
 
   if [ "$LATEST_PACKAGE_VERSION" = "$CURRENT_PACKAGE_VERSION" ]; then
     echo "Notice: This module is already published at $CURRENT_PACKAGE_VERSION. aborting publish."
