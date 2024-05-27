7595610907ed4f29370ca80bbb33aaad22ef6447
legobt <6wbvkn0j@anonaddy.me>
2024-05-27 01:49:31 +0000

    fix: install deps for legacy npm/legacy publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,15 +9,23 @@ YARN_MAJOR="$(yarn --version | sed 's/\..*//' || true)"
 if [[ "$YARN_MAJOR" -ge "3" ]]; then
   PUBLISH_CMD="yarn npm publish --tag $PUBLISH_NPM_TAG"
   PACK_CMD="yarn pack --out /tmp/%s-%v.tgz"
+  # install is handled by yarn berry pack/publish
+  INSTALL_CMD=""
 else
   echo "Warning: Did not detect compatible yarn version. This action officially supports Yarn v3 and newer. Falling back to using npm." >&2
   export npm_config__auth="$YARN_NPM_AUTH_TOKEN"
   PUBLISH_CMD="npm publish --tag $PUBLISH_NPM_TAG"
   PACK_CMD="npm pack --pack-destination=/tmp/"
+  if [[ -f 'yarn.lock' ]]; then
+    INSTALL_CMD="yarn install --frozen-lockfile"
+  else
+    INSTALL_CMD="npm ci"
+  fi
 fi
 
 if [[ -z $YARN_NPM_AUTH_TOKEN ]]; then
   echo "Notice: 'npm-token' not set. Running '$PACK_CMD'."
+  $INSTALL_CMD
   $PACK_CMD
   exit 0
 fi
@@ -48,4 +56,5 @@ if [[ -n "$1" ]]; then
   fi
 fi
 
+$INSTALL_CMD
 $PUBLISH_CMD
