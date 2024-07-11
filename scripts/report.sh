baad2066ee504ec12ebf33165db8a8da12892797
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-07-11 11:53:21 +0200

    Fix report script for unpublished packages

--- scripts/report.sh
+++ scripts/report.sh
@@ -11,6 +11,15 @@ for file in "$directory"/*.tgz; do
         echo "Processing $file"
         basename=$(basename "$file")
         name="$(tar -O -zxf "$file" package/package.json | jq --raw-output .name)"
+
+        LATEST_PACKAGE_VERSION=$(npm view "$name" dist-tags --workspaces false --json | jq --raw-output '.latest' || echo "")
+
+        # Skip if the package is not published
+        if [ -z "$LATEST_PACKAGE_VERSION" ]; then
+            echo "Skipping $file, because $name is not published on NPM"
+            continue
+        fi
+
         echo "$name"
         pkdiff "$name@latest" "$file" \
             --no-exit-code \
