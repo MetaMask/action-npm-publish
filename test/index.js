6364508c410e979b0c6df1f1aa1e3c9b75b017a7
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-05-27 10:54:48 +0200

    chore!: Drop support for Node.js <22 (#113)
    * Drop support for Node.js <22
    
    * Revert workflow changes
    
    * Bump ESLint and related dependencies
    
    * Fix Prettier config
    
    * Remove old ESLint config
    
    * Fix test
    
    * Trigger Socket
    
    * Remove --ext
    
    * Trigger CI

--- test/index.js
+++ test/index.js
@@ -1,54 +1,54 @@
 const { exec } = require('child_process');
-// eslint-disable-next-line node/no-unpublished-require
 const { test } = require('tapzero');
-const package = require('../package.json');
 
-const { devDependencies, version } = package;
+const packageJson = require('../package.json');
+
+const { devDependencies, version } = packageJson;
 const INVALID_TOKEN = 'should error when token is invalid';
 const FAKE = 'FAKE';
 
-test('should not have dependencies from now until forever', async (t) => {
+test('should not have dependencies from now until forever', async (tapzero) => {
   const depKey = 'dependencies';
-  const hasDeps = Object.prototype.hasOwnProperty.call(package, depKey);
-  t.equal(hasDeps, false);
+  const hasDeps = Object.prototype.hasOwnProperty.call(packageJson, depKey);
+  tapzero.equal(hasDeps, false);
 });
 
-test('should only have ten devDependency from now until forever', async (t) => {
+test('should only have 12 devDependency from now until forever', async (tapzero) => {
   const { length } = Object.keys(devDependencies);
-  t.equal(length, 10);
+  tapzero.equal(length, 12);
 });
 
-test('should not error when performing a dry-run publish', async (t) => {
+test('should not error when performing a dry-run publish', async (tapzero) => {
   await new Promise((resolve, reject) => {
     exec('./scripts/main.sh', (error) => {
       if (error) {
         reject(new Error(error));
       }
-      t.equal(error, null);
+      tapzero.equal(error, null);
       resolve();
     });
   });
 });
 
-test('correct version should appear in dry-run output', async (t) => {
+test('correct version should appear in dry-run output', async (tapzero) => {
   await new Promise((resolve, reject) => {
     exec('./scripts/main.sh', (error, stdout) => {
       if (error) {
         reject(new Error(error));
       }
-      t.equal(stdout.includes(version), true);
+      tapzero.equal(stdout.includes(version), true);
       resolve();
     });
   });
 });
 
-test('should error when token is invalid', async (t) => {
+test('should error when token is invalid', async (tapzero) => {
   await new Promise((resolve, reject) => {
     exec(`YARN_NPM_AUTH_TOKEN=${FAKE} ./scripts/main.sh`, (error) => {
       if (!error) {
         reject(new Error(INVALID_TOKEN));
       }
-      t.equal(error.code, 1);
+      tapzero.equal(error.code, 1);
       resolve();
     });
   });
