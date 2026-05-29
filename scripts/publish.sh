b9737a8eea157d0540377232b4727d8ad4388745
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-05-29 10:13:29 +0200

    Use `yarn npm publish --staged`

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,27 +4,18 @@ set -e
 set -o pipefail
 
 YARN_MAJOR="$(yarn --version | sed 's/\..*//' || true)"
-if [[ "$YARN_MAJOR" -ge "3" ]]; then
-  PUBLISH_CMD="yarn npm publish --tag $PUBLISH_NPM_TAG"
-  PACK_CMD="yarn pack --out /tmp/%s-%v.tgz"
-  # install is handled by yarn berry pack/publish
-  INSTALL_CMD=""
-else
-  echo "Warning: Did not detect compatible yarn version. This action officially supports Yarn v3 and newer. Falling back to using npm." >&2
-  PUBLISH_CMD="npm publish --tag $PUBLISH_NPM_TAG"
-  PACK_CMD="npm pack --pack-destination=/tmp/"
-  if [[ -f 'yarn.lock' ]]; then
-    INSTALL_CMD="yarn install --frozen-lockfile"
-  else
-    INSTALL_CMD="npm ci"
-  fi
+if [[ "$YARN_MAJOR" -lt "3" ]]; then
+  echo "Notice: Unsupported Yarn version: $YARN_MAJOR. Please upgrade to Yarn 3 or later."
+  exit 1
 fi
+
+PACK_CMD="yarn pack --out /tmp/%s-%v.tgz"
 
 # https://docs.npmjs.com/staged-publishing
 if [[ "$STAGED_PUBLISH" = "true" ]]; then
-  # Yarn does not support staged publishing (yet), so we use NPM for that even
-  # if Yarn is the default package manager.
-  PUBLISH_CMD="npm stage publish --tag $PUBLISH_NPM_TAG --provenance"
+  PUBLISH_CMD="yarn npm publish --tag $PUBLISH_NPM_TAG --staged --provenance"
+else
+  PUBLISH_CMD="yarn npm publish --tag $PUBLISH_NPM_TAG --provenance"
 fi
 
 # Perform a dry run if OIDC is not available.
@@ -58,7 +49,7 @@ fi
 CURRENT_PACKAGE_VERSION=$(jq --raw-output .version package.json)
 
 if [[ "$CURRENT_PACKAGE_VERSION" = "0.0.0" ]]; then
-  echo "Notice: Invalid version: $CURRENT_PACKAGE_VERSION. aborting publish."
+  echo "Notice: Invalid version: $CURRENT_PACKAGE_VERSION. Aborting publish."
   exit 0
 fi
 
@@ -81,5 +72,4 @@ if [[ -n "$IS_MONOREPO" ]]; then
   fi
 fi
 
-$INSTALL_CMD
 $PUBLISH_CMD
