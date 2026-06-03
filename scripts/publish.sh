8cc44c8407ea7e3da33eb7a04cb9e7bca67fff4c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-06-03 09:04:55 +0200

    Remove TODO for Yarn version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,8 +10,6 @@ fi
 check_requirements() {
   IFS='.' read -r YARN_MAJOR YARN_MINOR _ <<< "$(yarn --version)"
 
-  # TODO: Set this to the right version when Yarn releases a new version with the
-  # `--staged` flag for `yarn npm publish`.
   if [[ "$YARN_MAJOR" -lt 4 || ( "$YARN_MAJOR" -eq 4 && "$YARN_MINOR" -lt 16 ) ]]; then
     echo "::error::Yarn version 4.16.0 or higher is required. Detected version: $(yarn --version)."
     exit 1
